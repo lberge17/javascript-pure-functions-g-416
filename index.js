70dcb79be47dc38bcf744a09b470785fd0f4ae31
Laura Berge <lauraeberge@gmail.com>
2020-02-18 14:38:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much.";
+function addToPlaylist(playlist, episode){
+    return playlist.concat(episode);
+}
+
+function removeFromPlaylist(playlist, episode){
+    return playlist.filter(ep => ep !== episode);
+}
+
+function getNextEpisodeInPlaylist(playlist){
+    return playlist[0];
+}
+
+function bingeWatch(playlist){
+    if(playlist[0]){
+        playlist.shift();
+        bingeWatch(playlist);
+    } else{
+        return 'Please let there be more!'
+    }
+}
